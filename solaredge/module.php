53777808be0ba3e1e70223a2f90440db8c245d55
micserver <47324650+micserver@users.noreply.github.com>
2019-02-10 20:59:13 +0100

    Update module.php

--- solaredge/module.php
+++ solaredge/module.php
@@ -11,6 +11,9 @@
 			$this->RegisterPropertyInteger("SourceVariable", 0);
 			$this->RegisterPropertyString("Formula", "\$Value/10*sin(30)*pi()");
 			
+			$this->RegisterPropertyInteger("API_Key", "");
+			$this->RegisterPropertyInteger("ID", "");
+			
 			#$this->RegisterVariableFloat("GridPower", "Value", "", 0);
 			$this->RegisterVariableFloat("Value", "Value", "", 0);	
 		}
