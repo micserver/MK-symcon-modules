37d7febfe578a7eace92eafb3392a666bb6b3c8e
micserver <47324650+micserver@users.noreply.github.com>
2019-02-15 21:07:14 +0100

    Update module.php

--- solaredge/module.php
+++ solaredge/module.php
@@ -30,8 +30,9 @@
 			$archives = IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}");
 			$this->SendDebug("Archive ID: ",$archives[0],0);
 			$this->SendDebug("GridPower ID: ",$this->GetIDForIdent('GridPower'),0);
+			$this->SendDebug("GridPower ID: ",$this->GetIDForIdent('PV State'),0);
+			AC_SetLoggingStatus($archives[0], $this->GetIDForIdent('PV State'), true);
 			AC_SetLoggingStatus($archives[0], $this->GetIDForIdent('GridPower'), true);
-			
 		}
 	
 		/**
