836644100b765407bccb41687d1b47befaa7a9d8
micserver <47324650+micserver@users.noreply.github.com>
2019-02-22 11:16:24 +0100

    Update module.php

--- solaredge/module.php
+++ solaredge/module.php
@@ -30,31 +30,31 @@
 			//Set Logging Status
 			// Get ObjectID for first archive
 			$archives = IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}");
-			$this->SendDebug("Apply chnages => Archive ID: ",$archives[0],0);
-			$this->SendDebug("Apply chnages => Grid Power ID: ",$this->GetIDForIdent('GridPower'),0);
-			$this->SendDebug("Apply chnages => archive Grid Power: ",$this->ReadPropertyBoolean('GridPower'),0);
-			$this->SendDebug("Apply chnages => PV State ID: ",$this->GetIDForIdent('PV_State'),0);
-			$this->SendDebug("Apply chnages => archive PV State: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
+			$this->SendDebug("AC=> Archive ID: ",$archives[0],0);
+			$this->SendDebug("AC=> Grid Power ID: ",$this->GetIDForIdent('GridPower'),0);
+			$this->SendDebug("AC=> archive Grid Power: ",$this->ReadPropertyBoolean('archive_GridPower'),0);
+			$this->SendDebug("AC=> PV State ID: ",$this->GetIDForIdent('PV_State'),0);
+			$this->SendDebug("AC=> archive PV State: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
 			
 			//Logging Status setzen
 			switch ($this->ReadPropertyBoolean('archive_PV_State')){
 				case true:
 				AC_SetLoggingStatus($archives[0], $this->GetIDForIdent('PV_State'), true);
-				$this->SendDebug("Switch => Archive PV Status auf true: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
+				$this->SendDebug("Sw=> Archive PV Status true: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
 				break;
 				case false:
 				AC_SetLoggingStatus($archives[0], $this->GetIDForIdent('PV_State'), false);
-				$this->SendDebug("Switch => Archive PV Status auf false: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
+				$this->SendDebug("Sw=> Archive PV Status false: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
 				break;
 				default:
-				$this->SendDebug("Switch => war wohl nix: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
+				$this->SendDebug("Sw=> war wohl nix: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
 			}
 			if ($this->ReadPropertyBoolean('archive_GridPower')){
 				AC_SetLoggingStatus($archives[0], $this->GetIDForIdent('GridPower'), true);
-				$this->SendDebug("IF => Archive Grid Power auf true: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
+				$this->SendDebug("IF=> Archive Grid Power true: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
 			} else {
 				AC_SetLoggingStatus($archives[0], $this->GetIDForIdent('GridPower'), false);
-				$this->SendDebug("If => Archive Grid Power auf false: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
+				$this->SendDebug("If=> Archive Grid Power false: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
 			}
 			    
 			#$this->SendDebug("Archive PV Status: ",$this->ReadPropertyBoolean('archive_PV_State'),0);
