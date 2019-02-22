ec00f59a46aeac8afe6373930593719b6678ed3e
micserver <47324650+micserver@users.noreply.github.com>
2019-02-22 18:06:20 +0100

    Update module.php

--- solaredge/module.php
+++ solaredge/module.php
@@ -81,18 +81,18 @@
 			$this->SendDebug("PV State: ",$PV_State,0);
 			$Gridpower=$json->siteCurrentPowerFlow->PV->currentPower*1000; // PV - Current Power
 			$this->SendDebug("Grid Power: ",$Gridpower,0);
-			$Load_Status=$json->siteCurrentPowerFlow->LOAD->status; // Load - Status
-			$this->SendDebug("Load_Status: "$Load_Status,0);
-			$LOAD_currentPower=$json->siteCurrentPowerFlow->LOAD->currentPower*-1000; // Load - Current Power
-			$this->SendDebug("Grid Power: ",$LOAD_currentPower,0);
-			$Grid_Status=$json->siteCurrentPowerFlow->GRID->status; // Grid - Status
-			$this->SendDebug("Grid Power: ",$Grid_Status,0);
-			$Grid_CurrentPower= $json->siteCurrentPowerFlow->GRID->currentPower*1000; // Grid - Current Power
-			$this->SendDebug("Grid Power: ",$Grid_CurrentPower,0);
-			$connection_0_from=$json->siteCurrentPowerFlow->connections[0]->from; // Connections - From LOAD
-			$this->SendDebug("Grid Power: ",$connection_0_from,0);
-    			$connection_0_to=$json->siteCurrentPowerFlow->connections[0]->to; // Connections - From LOAD
-			$this->SendDebug("Grid Power: ",$connection_0_to,0);
+			#$Load_Status=$json->siteCurrentPowerFlow->LOAD->status; // Load - Status
+			#$this->SendDebug("Load_Status: "$Load_Status,0);
+			#$LOAD_currentPower=$json->siteCurrentPowerFlow->LOAD->currentPower*-1000; // Load - Current Power
+			#$this->SendDebug("Grid Power: ",$LOAD_currentPower,0);
+			#$Grid_Status=$json->siteCurrentPowerFlow->GRID->status; // Grid - Status
+			#$this->SendDebug("Grid Power: ",$Grid_Status,0);
+			#$Grid_CurrentPower= $json->siteCurrentPowerFlow->GRID->currentPower*1000; // Grid - Current Power
+			#$this->SendDebug("Grid Power: ",$Grid_CurrentPower,0);
+			#$connection_0_from=$json->siteCurrentPowerFlow->connections[0]->from; // Connections - From LOAD
+			#$this->SendDebug("Grid Power: ",$connection_0_from,0);
+    			#$connection_0_to=$json->siteCurrentPowerFlow->connections[0]->to; // Connections - From LOAD
+			#$this->SendDebug("Grid Power: ",$connection_0_to,0);
 			
 			SetValue($this->GetIDForIdent("GridPower"), $Gridpower);
 			SetValue($this->GetIDForIdent("PV_State"), $PV_State);
