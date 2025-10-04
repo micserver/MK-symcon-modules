61563cddd9475e19e2507e8ed8bca9993a97bdcc
micserver <47324650+micserver@users.noreply.github.com>
2025-10-04 20:39:28 +0200

    Update module.php

--- AbfallReminderMK/module.php
+++ AbfallReminderMK/module.php
@@ -43,7 +43,6 @@ class AbfallReminderMK extends IPSModule
     public function AbR_FetchMails()
     {
         $this->SendDebug("Start AbR_FetchMails", "Methode wurde aufgerufen", 0);
-        {
         $imapID = $this->ReadPropertyInteger("IMAP_InstanzID");
         if ($imapID == 0 || !IPS_InstanceExists($imapID)) {
             $this->LogMessage("IMAP-Instanz nicht konfiguriert.", KL_WARNING);
