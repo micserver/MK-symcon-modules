71515b2375f4c9a9c2700a80a9c367ca2033468c
micserver <47324650+micserver@users.noreply.github.com>
2025-10-05 15:29:59 +0200

    update

--- AbfallReminderMK/module.php
+++ AbfallReminderMK/module.php
@@ -69,7 +69,6 @@ class AbfallReminderMK extends IPSModule
         $abfallarten = json_decode($this->ReadPropertyString("Abfallarten"), true);
         $arten = array_map(function($a) { return preg_quote($a['art'], '/'); }, $abfallarten);
         $artenRegex = implode('|', $arten);
-        $this->SendDebug("Abfallarten", $artenRegex, 0);
     
         for ($i = 0; $i < count($mailarray); $i++) {
             $sender = $mailarray[$i]['SenderAddress'] ?? '';
