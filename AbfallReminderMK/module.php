81909c054a66207a5759b3383e8dad1dfd64fd0d
micserver <47324650+micserver@users.noreply.github.com>
2025-10-05 21:20:16 +0200

    Update module.php

--- AbfallReminderMK/module.php
+++ AbfallReminderMK/module.php
@@ -41,18 +41,16 @@ class AbfallReminderMK extends IPSModule
         $eventVarID = $this->ReadPropertyInteger("EventVariableID");
         if ($eventVarID > 0) {
             $eid = @IPS_GetObjectIDByIdent("IMAPLastMessageEvent", $this->InstanceID);
-            $eventScript = 'IPS_LogMessage("AbfallReminderMK", "Event ausgelöst für Variable ' . $eventVarID . '"); AbfallReminderMK_ARMK_FetchMails(' . $this->InstanceID . ');';
             if ($eid === false) {
                 $eid = IPS_CreateEvent(0); // 0 = Trigger
                 IPS_SetEventTrigger($eid, 1, $eventVarID); // 1 = bei Variablenänderung
                 IPS_SetParent($eid, $this->InstanceID);
                 IPS_SetName($eid, "IMAPLastMessageEvent");
                 IPS_SetEventActive($eid, true);
-                IPS_SetEventScript($eid, $eventScript);
+                IPS_SetEventScript($eid, 'AbfallReminderMK_ARMK_FetchMails(' . $this->InstanceID . ');');
             } else {
                 IPS_SetEventTrigger($eid, 1, $eventVarID);
                 IPS_SetEventActive($eid, true);
-                IPS_SetEventScript($eid, $eventScript);
             }
         }
 
@@ -82,11 +80,7 @@ class AbfallReminderMK extends IPSModule
 
     public function FetchMails()
     {
-    // Debug: Event-ID ausgeben
-    $eventVarID = $this->ReadPropertyInteger("EventVariableID");
-    $eid = @IPS_GetObjectIDByIdent("IMAPLastMessageEvent", $this->InstanceID);
-    IPS_LogMessage("AbfallReminderMK", "Manueller Abruf: EventVariableID=$eventVarID, EventID=" . ($eid !== false ? $eid : 'nicht vorhanden'));
-    // --- Fehlerbehandlung IMAP ---
+        // --- Fehlerbehandlung IMAP ---
         $e = error_get_last();
         if (!empty($e) && array_key_exists('message', $e)) {
             $Mail_Timeout_Counter = GetValue($this->GetIDForIdent("MailTimeoutCounter"));
