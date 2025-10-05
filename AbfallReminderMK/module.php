b1de6b34671ce56b5d9fa14e5ed066c9c750c93f
micserver <47324650+micserver@users.noreply.github.com>
2025-10-05 19:21:58 +0200

    Update module.php

--- AbfallReminderMK/module.php
+++ AbfallReminderMK/module.php
@@ -120,12 +120,12 @@ class AbfallReminderMK extends IPSModule
         if (count($gueltigeTreffer) > 0) {
             $anzeige = "";
                 $html = '<div style="font-family:Roboto,Arial,sans-serif; font-size:22px; font-weight:bold; color:#000; padding:8px;">';
-                $html .= '<table style="width:100%; border-collapse:collapse; color:#000; font-size:22px;">';
+                $html .= '<table style="width:100%; border-collapse:collapse; color:#000; font-size:22px; font-family:Roboto,Arial,sans-serif; font-weight:bold;">';
                 foreach ($gueltigeTreffer as $t) {
                     $anzeige .= "{$t['art']}\t{$t['datum']}\n";
                     $html .= '<tr>';
-                    $html .= '<td style="padding:4px;">' . htmlspecialchars($t['art']) . '</td>';
-                    $html .= '<td style="padding:4px;">' . htmlspecialchars($t['datum']) . '</td>';
+                    $html .= '<td style="padding:4px; font-family:Roboto,Arial,sans-serif; font-size:22px; font-weight:bold; color:#000;">' . htmlspecialchars($t['art']) . '</td>';
+                    $html .= '<td style="padding:4px; font-family:Roboto,Arial,sans-serif; font-size:22px; font-weight:bold; color:#000;">' . htmlspecialchars($t['datum']) . '</td>';
                     $html .= '</tr>';
                 }
                 $html .= '</table></div>';
