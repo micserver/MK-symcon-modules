a9454cfa81f57a7ca4e73012d2a0c759bf8a0d16
micserver <47324650+micserver@users.noreply.github.com>
2025-10-05 20:14:04 +0200

    test

--- AbfallReminderMK/module.php
+++ AbfallReminderMK/module.php
@@ -5,15 +5,21 @@ class AbfallReminderMK extends IPSModule
 {
     public function Create()
     {
-        // Event für IMAP-LastMessage
-        $eid = @IPS_GetObjectIDByIdent("IMAPLastMessageEvent", $this->InstanceID);
-        if ($eid === false) {
-            $eid = IPS_CreateEvent(0); // 0 = Trigger
-            IPS_SetEventTrigger($eid, 1, 37572); // 1 = bei Variablenänderung
-            IPS_SetParent($eid, $this->InstanceID);
-            IPS_SetName($eid, "IMAPLastMessageEvent");
-            IPS_SetEventActive($eid, true);
-            IPS_SetEventScript($eid, 'AbfallReminderMK_ARMK_FetchMails(' . $this->InstanceID . ');');
+        // Event für konfigurierbare Variable
+        $eventVarID = $this->ReadPropertyInteger("EventVariableID");
+        if ($eventVarID > 0) {
+            $eid = @IPS_GetObjectIDByIdent("IMAPLastMessageEvent", $this->InstanceID);
+            if ($eid === false) {
+                $eid = IPS_CreateEvent(0); // 0 = Trigger
+                IPS_SetEventTrigger($eid, 1, $eventVarID); // 1 = bei Variablenänderung
+                IPS_SetParent($eid, $this->InstanceID);
+                IPS_SetName($eid, "IMAPLastMessageEvent");
+                IPS_SetEventActive($eid, true);
+                IPS_SetEventScript($eid, 'AbfallReminderMK_ARMK_FetchMails(' . $this->InstanceID . ');');
+            } else {
+                IPS_SetEventTrigger($eid, 1, $eventVarID);
+                IPS_SetEventActive($eid, true);
+            }
         }
         parent::Create();
         $this->LogMessage("Modul wurde geladen!", KL_NOTIFY);
@@ -36,8 +42,9 @@ class AbfallReminderMK extends IPSModule
             ["art" => "Gelber Sack"]
         ]));
         $this->RegisterPropertyString("OrtFilter", "Krombach");
-    $this->RegisterPropertyString("Testdatum", "2025-10-01");
+        $this->RegisterPropertyString("Testdatum", "2025-10-01");
         $this->RegisterPropertyInteger("FetchInterval", 3600);
+        $this->RegisterPropertyInteger("EventVariableID", 0);
 
         // Variablen
         $this->RegisterVariableString("AnzeigenText", "AnzeigenText", "~TextBox", 10);
@@ -51,10 +58,14 @@ class AbfallReminderMK extends IPSModule
 
     public function ApplyChanges()
     {
-        // Event bei Variablenänderung aktivieren
+        // Event bei Variablenänderung aktivieren (nur wenn konfiguriert)
+        $eventVarID = $this->ReadPropertyInteger("EventVariableID");
         $eid = @IPS_GetObjectIDByIdent("IMAPLastMessageEvent", $this->InstanceID);
-        if ($eid !== false) {
+        if ($eid !== false && $eventVarID > 0) {
+            IPS_SetEventTrigger($eid, 1, $eventVarID);
             IPS_SetEventActive($eid, true);
+        } else if ($eid !== false && $eventVarID == 0) {
+            IPS_SetEventActive($eid, false);
         }
         parent::ApplyChanges();
     // Aktion für manuelles ARMKufen wird über form.json und RequestAction behandelt
