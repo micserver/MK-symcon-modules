605df80983863cb1b57f40fb7395c8e4fddfe183
micserver <47324650+micserver@users.noreply.github.com>
2025-10-06 20:25:38 +0200

    t

--- TestMK/module.php
+++ TestMK/module.php
@@ -20,61 +20,42 @@ class TestMK extends IPSModule
 
     public function Create()
     {
-    $this->RegisterPropertyInteger("EventVariableID", 0);
-    $this->EnableAction("LogEventDebug");
-    $this->EnableAction("TestEvent");
+        $this->RegisterPropertyInteger("EventVariableID", 0);
+        $this->EnableAction("LogEventDebug");
+        $this->EnableAction("TestEvent");
         $this->SendDebug("Create", "EventVariableID=" . $this->ReadPropertyInteger("EventVariableID"), 0);
         parent::Create();
-        // Event für konfigurierbare Variable
-        $eventVarID = $this->ReadPropertyInteger("EventVariableID");
-        if ($eventVarID > 0) {
-            $eid = @IPS_GetObjectIDByIdent("TestMKEvent", $this->InstanceID);
-            $eventScript = 'IPS_LogMessage("TestMK", "Event ausgelöst für Variable ' . $eventVarID . '");';
-            if ($eid === false) {
-                $eid = IPS_CreateEvent(0); // 0 = Trigger
-                IPS_SetEventTrigger($eid, 1, $eventVarID); // 1 = bei Variablenänderung
-                IPS_SetParent($eid, $this->InstanceID);
-                IPS_SetName($eid, "TestMKEvent");
-                IPS_SetEventActive($eid, true);
-                IPS_SetEventScript($eid, $eventScript);
-            } else {
-                IPS_SetEventTrigger($eid, 1, $eventVarID);
-                IPS_SetEventActive($eid, true);
-                IPS_SetEventScript($eid, $eventScript);
-            }
-        }
     }
 
     public function ApplyChanges()
     {
-    
-    
         $eventVarID = $this->ReadPropertyInteger("EventVariableID");
         $this->SendDebug("ApplyChanges", "EventVariableID=" . $eventVarID, 0);
-        // Alle Events mit Name "TestMKEvent" unterhalb der Instanz löschen
-        $children = IPS_GetChildrenIDs($this->InstanceID);
-        foreach ($children as $childID) {
-            if (IPS_GetObject($childID)['ObjectType'] == 4) { // 4 = Event
-                if (IPS_GetObject($childID)['ObjectName'] == "TestMKEvent") {
-                    IPS_DeleteEvent($childID);
-                    $this->SendDebug("ApplyChanges", "Doppeltes Event gelöscht: EventID=$childID", 0);
-                }
-            }
-        }
-        // Neues Event nur anlegen, wenn Variable > 0
-        if ($eventVarID > 0) {
-            $eid = IPS_CreateEvent(0); // 0 = Trigger
-            IPS_SetParent($eid, $this->InstanceID);
-            IPS_SetName($eid, "TestMKEvent");
-            IPS_SetIdent($eid, "TestMKEvent");
-            IPS_SetEventTrigger($eid, 1, $eventVarID);
-            IPS_SetEventActive($eid, true);
-            // Event-Script ruft die Debug-Funktion des Moduls per RequestAction auf
-            $eventScript = 'IPS_RequestAction(' . $this->InstanceID . ', "LogEventDebug", ' . $eventVarID . ');';
-            IPS_SetEventScript($eid, $eventScript);
-            $this->SendDebug("ApplyChanges", "Neues Event angelegt: EventID=$eid für Variable $eventVarID", 0);
+        // MessageSink: Variable für VM_UPDATE registrieren
+        $this->UnregisterAllMessages();
+        if ($eventVarID > 0 && IPS_VariableExists($eventVarID)) {
+            $this->RegisterMessage($eventVarID, 10603); // 10603 = VM_UPDATE
+            $this->SendDebug("ApplyChanges", "MessageSink für Variable $eventVarID registriert", 0);
         }
         parent::ApplyChanges();
+    }
+
+    private function UnregisterAllMessages()
+    {
+        $children = IPS_GetChildrenIDs($this->InstanceID);
+        foreach ($children as $childID) {
+            if (IPS_GetObject($childID)['ObjectType'] == 2) { // 2 = Variable
+                $this->UnregisterMessage($childID, 10603);
+            }
+        }
+    }
+
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+    {
+        if ($Message == 10603) { // VM_UPDATE
+            $this->SendDebug("MessageSink", "Variable $SenderID geändert: Neuer Wert=" . GetValue($SenderID), 0);
+            // Hier kannst du beliebige Reaktionen einbauen
+        }
     }
 
     public function TestEvent()
