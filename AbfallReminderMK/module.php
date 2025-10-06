5c65c36fc749565d242e8bf8c764b6add2eeb246
micserver <47324650+micserver@users.noreply.github.com>
2025-10-06 19:17:19 +0200

    ev 9

--- AbfallReminderMK/module.php
+++ AbfallReminderMK/module.php
@@ -254,7 +254,7 @@ class AbfallReminderMK extends IPSModule
 
 }
 
-// Wrapper-Funktion für Timer (muss außerhalb der Klasse stehen!)
+// Wrapper-Funktion für Timer (muss außerhalb der Klasse  stehen!)
 function AbfallReminderMK_ARMK_FetchMails($InstanceID) {
     IPS_RequestAction($InstanceID, "FetchMails", "");
 }
