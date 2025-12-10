0578a35523196bf07da3827ac3cf71f2933a5e50
micserver <47324650+micserver@users.noreply.github.com>
2025-12-10 18:09:43 +0100

    t

--- AbfallReminderMK/module.php
+++ AbfallReminderMK/module.php
@@ -229,9 +229,9 @@ class AbfallReminderMK extends IPSModule
                 
                 $anzeige .= "{$t['art']}\t{$kurzdatum}\n";
                 
-                $html .= '<tr>';
-                $html .= '<td style="padding:0; font-family:Roboto,Arial,sans-serif; font-size:20px; font-weight:bold; color:#000; width:70%;">' . htmlspecialchars($t['art']) . '</td>';
-                $html .= '<td style="padding:0; font-family:Roboto,Arial,sans-serif; font-size:20px; font-weight:bold; color:#000; text-align:right; width:30%;">' . htmlspecialchars($kurzdatum) . '</td>';
+                $html .= '<tr style="height:100%;">';
+                $html .= '<td style="padding:0; font-family:Roboto,Arial,sans-serif; font-size:20px; font-weight:bold; color:#000; width:70%; vertical-align:middle;">' . htmlspecialchars($t['art']) . '</td>';
+                $html .= '<td style="padding:0; font-family:Roboto,Arial,sans-serif; font-size:20px; font-weight:bold; color:#000; text-align:right; width:30%; vertical-align:middle;">' . htmlspecialchars($kurzdatum) . '</td>';
                 $html .= '</tr>';
             }
             $html .= '</table></div>';
